0648eca15d7b128f23b31145347b3f2604487e6a
Frederick MARINHO <frmarinh@e1r10p1.42.fr>
2018-06-12 18:10:12 +0200

    uhhhhhh

--- transactionBuilder/srcs/app.js
+++ transactionBuilder/srcs/app.js
@@ -1,4 +1,5 @@
 const rp = require("request-promise");
+const sha256 = require("sha256");
 
 var EC = require('elliptic').ec;
 var ec = new EC('secp256k1');
@@ -6,7 +7,7 @@ var ec = new EC('secp256k1');
 let pub = "04c92b79fef5883a11e16bd2ce3f6626832031decb8fb85193c11947af32dac2974e8ef77b266900bfb6904251cb9f06fe27da473232ef972a3ed0ad80bac27d79";
 let priv = "7911f87905f2165c75b02077caaf7f59e99a0ff14a05b8ee3e401fa6eb6b7373";
 
-let node = "http://localhost:3002";
+let node = "http://localhost:3000";
 
 let amount = process.argv[2];
 let recipient = process.argv[3];
@@ -14,11 +15,13 @@ let recipient = process.argv[3];
 let transaction = {
     amount: amount,
     recipient: recipient,
-    sender: pub
+    sender: pub,
+    initiated: Date.now()
 }
 
+let hash = sha256(JSON.stringify(transaction));
 let key = ec.keyFromPrivate(priv);
-transaction.signature = key.sign(Buffer.from(JSON.stringify(transaction))).toDER();
+transaction.signature = key.sign(hash).toDER();
 
 rp({
     method: "POST",
@@ -27,6 +30,5 @@ rp({
     json: true,
     timeout: 1500
 }).then((answer) => {console.log(answer);}).catch(() => {});
-
 
 console.log(transaction, `sent to node ${node}`);
